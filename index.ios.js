b8d86d9912546216f2d680889c736fa3d9614671
Ryan James <ryanwilsonjames@gmail.com>
2017-02-07 13:50:26 -0800

    BUTTS

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Image, Text } from 'react-native';
 
 class FirstTutorial extends Component {
   render() {
+    const tinaBelcher = { uri: 'https://s-media-cache-ak0.pinimg.com/736x/f7/86/ed/f786ed81b3e25f10b0bb024712f7d544.jpg' };
+
     return (
-      <Text style={ { margin: 50 } }>
-        Sup world!
-      </Text>
+//      <Text style={ { margin: 50 } }>
+//        Sup world!
+//      </Text>
+      <Image source={tinaBelcher} style={ { flex: 1 } } />
     );
   }
 }
