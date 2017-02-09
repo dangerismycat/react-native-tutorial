f3687b4c5f65bbe7afae6bf325f76a3da85c4ba1
Ryan James <ryanwilsonjames@gmail.com>
2017-02-09 12:01:23 -0800

    hey look, I brought back the <blink> tag

--- index.ios.js
+++ index.ios.js
@@ -2,6 +2,34 @@ import React from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
 
+class Blink extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = { visible: true };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ visible: !this.state.visible });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const content = this.state.visible ? this.props.children : null;
+
+    return (
+      <View style={ { alignItems: this.props.align } }>
+        {content}
+      </View>
+    );
+  }
+}
+
 class Greeting extends React.Component {
   render() {
     return (
@@ -28,11 +56,13 @@ class FirstTutorial extends React.Component {
     return (
       <View style={ { alignItems: 'center' } }>
         <Buster />
-        <Greeting target={'brother'} />
-        <Greeting target={'hermano'} />
-        <Greeting target={'possible nephew'} />
-        <Greeting target={'fake uncle Jack'} />
-        <Greeting target={'father / uncle dad'} />
+        <Blink align={'center'}>
+          <Greeting target={'brother'} />
+          <Greeting target={'hermano'} />
+          <Greeting target={'possible nephew'} />
+          <Greeting target={'fake uncle Jack'} />
+          <Greeting target={'father / uncle dad'} />
+        </Blink>
       </View>
     );
   }
