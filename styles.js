fd5ed3a750446ed3751ec214edd7e275933a73d8
Ryan James <ryanwilsonjames@gmail.com>
2017-02-14 08:35:38 -0800

    we have squares

--- styles.js
+++ styles.js
@@ -12,8 +12,32 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     marginTop: 30,
   },
+  'color-brick-red': {
+    backgroundColor: '#d63d22'
+  },
+  'color-mid-blue': {
+    backgroundColor: '#26a4f2'
+  },
+  'color-spring-green': {
+    backgroundColor: '#27dd49'
+  },
   greeting: {
     marginTop: 15,
+  },
+  'square-small': {
+    height: 10,
+    width: 10,
+    margin: 10,
+  },
+  'square-medium': {
+    height: 25,
+    width: 25,
+    margin: 10,
+  },
+  'square-large': {
+    height: 50,
+    width: 50,
+    margin: 10,
   },
 });
 
