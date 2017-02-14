152def0bb88ada89f3d6abab1e404917e2784d85
Ryan James <ryanwilsonjames@gmail.com>
2017-02-14 09:57:38 -0800

    direction, justify, align

--- styles.js
+++ styles.js
@@ -26,25 +26,31 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   'square-small': {
-    // height: 10,
-    // width: 10,
-    flex: 1,
+    height: 10,
+    width: 10,
+    // flex: 1,
   },
   'square-medium': {
-    // height: 25,
-    // width: 25,
-    flex: 2,
+    height: 25,
+    width: 25,
+    // flex: 2,
   },
   'square-large': {
-    // height: 50,
-    // width: 50,
-    flex: 3,
+    height: 50,
+    width: 50,
+    // flex: 3,
   },
   squaresContainer: {
-    // flex: 1,
+    flex: 1,
+    // column, row
+    flexDirection: 'column',
+    // flex-start, center, flex-end, space-around, space-between
+    justifyContent: 'center',
+    // flex-start, center, flex-end, stretch
+    alignItems: 'flex-end',
     marginTop: 10,
-    height: 300,
-    width: 300,
+    // height: 300,
+    // width: 300,
   },
 });
 
